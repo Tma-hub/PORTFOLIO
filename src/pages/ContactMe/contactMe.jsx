f44bc731c00fc71deeda9bb6892afcf721c893de
Tma-hub <tereza.matousova@gmail.com>
2026-05-25 08:47:49 +0000

    home button

--- src/pages/ContactMe/contactMe.jsx
+++ src/pages/ContactMe/contactMe.jsx
@@ -1,4 +1,5 @@
 import "./contactMe.css";
+import { Link } from "react-router-dom";
 
 import instagramIcon from "../../img/instagram.png";
 import linkedinIcon from "../../img/linkedin.png";
@@ -6,33 +7,34 @@ import mailIcon from "../../img/mail.png";
 
 export default function ContactMe() {
   return (
-    <main className="contact-page">
-      <section className="contact-icons">
-        <a
-          href="https://www.instagram.com/tereza.matous/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="Instagram"
-        >
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
+    <div className="contact-wrapper">
+      <header className="navbar">
+        <Link to="/" className="logo">
+          Tereza Matousova
+        </Link>
 
-        <a
-          href="https://www.linkedin.com/in/tereza-matoušová-b16477178"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="LinkedIn"
-        >
-          <img src={linkedinIcon} alt="LinkedIn" />
-        </a>
+        <nav className="nav-links">
+          <Link to="/about">About</Link>
+          <Link to="/portfolio">Work</Link>
+          <Link to="/contact">Contact me</Link>
+        </nav>
+      </header>
 
-        <a
-          href="mailto:tereza.matousova@gmail.com"
-          aria-label="Email"
-        >
-          <img src={mailIcon} alt="Email" />
-        </a>
-      </section>
-    </main>
+      <main className="contact-page">
+        <section className="contact-icons">
+          <a href="https://www.instagram.com/tereza.matous/" target="_blank" rel="noreferrer">
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+
+          <a href="https://www.linkedin.com/in/tereza-matoušová-b16477178" target="_blank" rel="noreferrer">
+            <img src={linkedinIcon} alt="LinkedIn" />
+          </a>
+
+          <a href="mailto:tereza.matousova@gmail.com">
+            <img src={mailIcon} alt="Email" />
+          </a>
+        </section>
+      </main>
+    </div>
   );
 }
