ca4d133e6f885f41c82ad867c23b020b2def1ba6
Tma-hub <tereza.matousova@gmail.com>
2026-05-25 08:36:03 +0000

    contact

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,5 @@
-import './style.css';
-import { Link } from 'react-router-dom';
-import './style.css';
+import "./style.css";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
