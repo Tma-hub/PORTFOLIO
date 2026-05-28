7c7deb0df56d08145f1becd7e6588813642d8940
Tma-hub <tereza.matousova@gmail.com>
2026-05-28 16:05:50 +0000

    dark mode

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,36 @@
-import "./style.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import "./style.css";
+
+import lightOn from "../../img/light_on.png";
+import darkOn from "../../img/dark_on.png";
 
 export default function HomePage() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   return (
     <div className="home">
       <header className="navbar">
         <div className="logo">Tereza Matousova</div>
 
         <nav className="nav-links">
+          <div
+            className="theme-toggle"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            <span>{darkMode ? "Dark mode" : "Light mode"}</span>
+            <img src={darkMode ? darkOn : lightOn} alt="theme toggle" />
+          </div>
+
           <Link to="/about">About</Link>
           <Link to="/portfolio">Work</Link>
           <Link to="/contact">Contact me</Link>
